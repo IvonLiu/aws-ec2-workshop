b07f5e032d54510f05f9dc057897dff3b3871712
Ivon Liu <me@ivonliu.com>
2017-04-27 17:57:21 -0700

    Change url

--- workshop/client/js/main.js
+++ workshop/client/js/main.js
@@ -1,5 +1,5 @@
 /* global $ */
-var url_base = /* TODO: Replace with EC2 instance URL */;
+var url_base = 'http://ec2-54-214-225-82.us-west-2.compute.amazonaws.com';
 
 class Main {
     constructor() {
